b465ee17778d723fc743a070988251b426d7dc99
jasneet <jasneet2@illinois.edu>
2018-09-26 20:21:16 -0500

    Done!

--- src/main/java/Colosseum.java
+++ src/main/java/Colosseum.java
@@ -90,7 +90,14 @@ public class Colosseum {
      * Implement this function.
      */
     public static void printWhoIsAhead() {
-        System.out.println("Implement me!");
+
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints) {
+            System.out.println(firstPokemon.name + " is currently ahead!");
+        } else if (secondPokemon.hitPoints > firstPokemon.hitPoints) {
+            System.out.println(secondPokemon.name + " is currently ahead!");
+        } else {
+            System.out.println(firstPokemon.name + " " + secondPokemon.name + " are tied!");
+        }
     }
 
     /**
@@ -101,7 +108,13 @@ public class Colosseum {
      * Write this function.
      */
     public static void determineWinner() {
-        System.out.println("Implement me!");
+
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints) {
+            System.out.println(firstPokemon.name + " is the winner!");
+        } else {
+            System.out.println(secondPokemon.name + " is the winner ahead!");
+        }
+
     }
 
     /**
